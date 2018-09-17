4e1bb30a2a082b9e82ef9a821a06202f5280d5c8
Shannon Clarke <shannonajclarke@gmail.com>
2018-09-17 05:59:18 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,33 @@
+// our dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+//Config bodyparser to parse JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// from top level path e.g. localhost:3000, this response will be sent
+app.get('/', (request, response) => response.send('Hello World'));
+
+//Basic GET request
+app.get("/hello", function(req, res) {
+  if(!req.query.name) {
+      return res.send({"status": "error", "message": "missing a parameter"});
+  } else {
+      return res.send("Welcome back, "+req.query.name);
+  }
+});
+
+//Basic POST request
+app.post("/hello", function(req, res) {
+  if(!req.body.name) {
+      return res.send({"status": "error", "message": "missing a parameter"});
+  } else {
+      return res.send("Welcome back, "+req.body.name);
+  }
+});
+
+// set the server to listen on port 3000
+app.listen(3000, () => console.log('Listening on port 3000'));
